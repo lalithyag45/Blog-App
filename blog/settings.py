2b5c50a5316bdd44cec991903641ee6e2243487a
lalithyag45 <92256236+lalithyag45@users.noreply.github.com>
2022-09-02 20:26:07 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
